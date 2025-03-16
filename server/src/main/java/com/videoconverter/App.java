dd4092ec4920c77b00043cea1c364aae22958750
Alex Oliveira <alexoliveira7x@gmail.com>
2025-03-16 17:16:52 +0000

    feat: write file to uploads folder

--- server/src/main/java/com/videoconverter/App.java
+++ server/src/main/java/com/videoconverter/App.java
@@ -3,6 +3,8 @@ package com.videoconverter;
 import io.javalin.Javalin;
 import io.javalin.http.UploadedFile;
 
+import java.io.*;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,8 +20,25 @@ public class App
 
             if (file == null) {
                 errors.put("file", "file is required");
-
                 context.status(422).json(errors);
+            } else {
+                File uploadDir = new File("uploads");
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdirs();
+                }
+                File targetFile = new File(uploadDir, file.filename());
+
+                try (InputStream input = file.content()) {
+                    OutputStream output = new FileOutputStream(targetFile);
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = input.read(buffer)) != -1) {
+                        output.write(buffer, 0, bytesRead);
+                    }
+
+                    context.status(201);
+                }
             }
         });
     }
